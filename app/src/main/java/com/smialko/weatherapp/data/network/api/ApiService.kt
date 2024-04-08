628347088474f77823576cac5a6c2bbd92f79ed2
vova77 <vova2000smi@gmail.com>
2024-04-08 21:43:14 +0300

    Add interceptor

--- app/src/main/java/com/smialko/weatherapp/data/network/api/ApiService.kt
+++ app/src/main/java/com/smialko/weatherapp/data/network/api/ApiService.kt
@@ -10,18 +10,18 @@ import retrofit2.http.Query
 interface ApiService {
 
 
-    @GET("current.json?")
+    @GET("current.json")
     suspend fun loadCurrentWeather(
         @Query("q") query: String
     ): WeatherCurrentDto
 
-    @GET("forecast.json?")
+    @GET("forecast.json")
     suspend fun loadForecast(
         @Query("q") query: String,
         @Query("days") daysCount: Int = 4
     ): WeatherForecastDto
 
-    @GET("search.json?")
+    @GET("search.json")
     suspend fun searchCity(
         @Query("q") query: String
     ): List<CityDto>
