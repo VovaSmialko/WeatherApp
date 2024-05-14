ac30e42e561eec1bac21b6a4c275c7d70d8a0e40
vova77 <vova2000smi@gmail.com>
2024-05-14 21:04:55 +0300

    Added min/max temp in forecast cards

--- app/src/main/java/com/smialko/weatherapp/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/smialko/weatherapp/data/repository/WeatherRepositoryImpl.kt
@@ -1,17 +1,20 @@
 package com.smialko.weatherapp.data.repository
 
+import android.util.Log
 import com.smialko.weatherapp.data.mapper.toEntity
 import com.smialko.weatherapp.data.network.api.ApiService
 import com.smialko.weatherapp.domain.entity.Forecast
 import com.smialko.weatherapp.domain.entity.Weather
 import com.smialko.weatherapp.domain.entity.repository.WeatherRepository
 import javax.inject.Inject
+import kotlin.math.log
 
 class WeatherRepositoryImpl @Inject constructor(
     private val apiService: ApiService
 ) : WeatherRepository {
     override suspend fun getWeather(cityId: Int): Weather {
         return apiService.loadCurrentWeather("$PREFIX_CITY_ID$cityId").toEntity()
+
     }
 
     override suspend fun getForecast(cityId: Int): Forecast {
--- app/src/main/java/com/smialko/weatherapp/domain/entity/Weather.kt
+++ app/src/main/java/com/smialko/weatherapp/domain/entity/Weather.kt
@@ -4,6 +4,8 @@ import java.util.Calendar
 
 data class Weather(
     val tempC: Float,
+    val minTempC: Float?,
+    val maxTempC: Float?,
     val conditionText: String,
     val conditionUrl: String,
     val date: Calendar
